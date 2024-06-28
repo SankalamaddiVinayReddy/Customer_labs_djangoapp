a9489b89a1c48336ac80d3561a6db76184dcd381
SankalamaddiVinayReddy <143685873+SankalamaddiVinayReddy@users.noreply.github.com>
2024-06-28 17:02:09 +0530

    Update models.py
    Just updated

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,11 @@ import uuid
 
 class Account(models.Model):
     account_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+ #user account id
     email = models.EmailField(unique=True)
+ #email should be unique
     account_name = models.CharField(max_length=255)
+ #account name can be anything
     app_secret_token = models.CharField(max_length=255, unique=True, editable=False)
     website = models.URLField(blank=True, null=True)
 
